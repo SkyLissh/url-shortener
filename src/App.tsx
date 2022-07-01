dd7b44201bc486481f3c1b6efdb6b1d5dab224b3
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-07-01 16:02:46 -0500

    :tada: Init URL Shortener Frontend
    Initialize a simple frontend to use my api created on python

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+export default function App() {
+	return (
+		<main>
+			<h1>Create Short Links!</h1>
+			<p>
+				This is a simple tool, to create shorter links that are more readable, this
+				permits to the user have a better experience across the web.
+			</p>
+
+			<form>
+				<label>
+					<input type="text" placeholder="Paste a link to shorten it!" />
+				</label>
+			</form>
+		</main>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html", "./src/**/*.{ts, tsx}"],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		/**
+		 * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+		 * for forms. If you don't like it or have own styling for forms,
+		 * comment the line below to disable '@tailwindcss/forms'.
+		 */
+		require("@tailwindcss/forms")
+	]
+};
